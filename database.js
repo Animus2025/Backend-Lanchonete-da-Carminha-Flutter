4f8098f20ed7267bee896c1fccd700264112b028
João Henrique <jhaguiar1@outlook.com>
2025-06-21 14:43:02 -0300

    Update database.js

--- database.js
+++ database.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2'); // Importa o módulo mysql2, que permite conect
 const db = mysql.createConnection({
   host: 'localhost',        // Endereço do servidor MySQL (localhost = máquina local)
   user: 'root',             // Usuário do banco de dados (padrão do MySQL é 'root')
-  password: '123456',       // Senha do usuário do banco de dados
-  database: 'lanchonete_carminha' // Nome do banco de dados que será utilizado
+  password: '2104',       // Senha do usuário do banco de dados
+  database: 'lanchonete' // Nome do banco de dados que será utilizado
 });
 
 // Tenta estabelecer a conexão com o banco de dados
