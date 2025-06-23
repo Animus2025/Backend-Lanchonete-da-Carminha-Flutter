e9eeef575c4f211b1fb469db0df23d42a0241a72
Luana Fialho <luanafialho140@gmail.com>
2025-06-23 02:24:12 -0300

    Atualizando banco de dados

--- database/backup_lanchonete.sql
+++ database/backup_lanchonete.sql
@@ -16,29 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `codigos`
---
-
-DROP TABLE IF EXISTS `codigos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `codigos` (
-  `numero` text,
-  `codigo` text,
-  `criadoEm` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `codigos`
---
-
-LOCK TABLES `codigos` WRITE;
-/*!40000 ALTER TABLE `codigos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `codigos` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `pagamento`
 --
 
@@ -51,8 +28,8 @@ CREATE TABLE `pagamento` (
   `status_pagamento` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_pagamento`),
   UNIQUE KEY `id_pedido` (`id_pedido`),
-  CONSTRAINT `pagamento_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `fk_pagamento_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -61,6 +38,7 @@ CREATE TABLE `pagamento` (
 
 LOCK TABLES `pagamento` WRITE;
 /*!40000 ALTER TABLE `pagamento` DISABLE KEYS */;
+INSERT INTO `pagamento` VALUES (13,17,'Pendente'),(14,18,'Pendente'),(15,19,'Pendente'),(16,20,'Pendente');
 /*!40000 ALTER TABLE `pagamento` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,10 +57,12 @@ CREATE TABLE `pedido` (
   `horario_retirada` time DEFAULT NULL,
   `forma_pagamento` varchar(50) DEFAULT NULL,
   `status` varchar(50) DEFAULT NULL,
+  `tipo_pagamento` varchar(50) DEFAULT NULL,
+  `valor_total` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`id_pedido`),
   KEY `id_usuario` (`id_usuario`),
   CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,6 +71,7 @@ CREATE TABLE `pedido` (
 
 LOCK TABLES `pedido` WRITE;
 /*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
+INSERT INTO `pedido` VALUES (17,21,'2025-06-23 01:58:46','2025-06-25','19:00:00','pix','Cancelado','integral',93.00),(18,21,'2025-06-23 02:01:52','2025-06-28','08:00:00','pix','Cancelado','integral',15.60),(19,21,'2025-06-23 02:15:09','2025-07-05','20:00:00','pix','Pendente','50%',211.20),(20,21,'2025-06-23 02:18:51','2025-06-25','20:00:00','pix','Pendente','integral',54.00);
 /*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -107,12 +88,13 @@ CREATE TABLE `pedido_produto` (
   `id_produto` int DEFAULT NULL,
   `quantidade` int DEFAULT NULL,
   `preco_unitario` decimal(10,2) DEFAULT NULL,
+  `estado` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `id_pedido` (`id_pedido`),
   KEY `id_produto` (`id_produto`),
-  CONSTRAINT `pedido_produto_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`),
+  KEY `fk_pedidoproduto_pedido` (`id_pedido`),
+  CONSTRAINT `fk_pedidoproduto_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`) ON DELETE CASCADE,
   CONSTRAINT `pedido_produto_ibfk_2` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id_produto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -121,6 +103,7 @@ CREATE TABLE `pedido_produto` (
 
 LOCK TABLES `pedido_produto` WRITE;
 /*!40000 ALTER TABLE `pedido_produto` DISABLE KEYS */;
+INSERT INTO `pedido_produto` VALUES (27,17,1,30,0.90,NULL),(28,17,4,30,0.90,NULL),(29,17,7,30,0.90,NULL),(30,17,14,1,12.00,NULL),(31,18,1,10,0.78,NULL),(32,18,2,10,0.78,NULL),(33,19,11,100,0.48,NULL),(34,19,10,200,0.48,NULL),(35,19,12,140,0.48,NULL),(36,20,1,60,0.90,NULL);
 /*!40000 ALTER TABLE `pedido_produto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -173,7 +156,7 @@ CREATE TABLE `usuario` (
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `telefone` (`telefone`),
   UNIQUE KEY `cpf` (`cpf`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -182,7 +165,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'LUANA','luana@gmail.com','Testando*','31971454673','BENJAMIN NEVES','22255588890'),(2,'lucas','lucas@gmail.com','Testando2*','3199999999','Benjamin freitas','64563563569'),(8,'Luana Test','carminha3@example.com','$2b$10$x8dvmTwoj93nZ8gGvjXcy.sp.y.IdOXqSq7Xic311kBtykKNvoxDu','11999999999','Rua das Delícias, 123','12345678900'),(10,'luana','luana2@gmail.com','$2b$10$YJcRw1d31RlmxPMjtDQl2ul9kFCoCAxfPlAe.H5ub7YEEEl6DNT/m','31900332244','nao sei nao em','12340987654'),(11,'julia freitas',NULL,'$2b$10$7AG63EzNWB3sMK.kmrwzVOXJrXy.W4qQDtdOeT7je1EbiSMtRcrve','31988005544','rua nao sei das quantas','78978978978'),(12,'leandra arruda',NULL,'$2b$10$uBQwEgTYQwB7WAZxw3WQ5O/1o5JEU0pdnSyl8h2xRJFf9K7EakFSG','41999999999','tqatatata','12312312312'),(13,'juliana dark',NULL,'$2b$10$iRycEJed8AmVYC59pLV2A.XDeEDVuS7.zB/Bxqtkw39cTfeIuO6Vq','51999999999','tatatat','67867867867'),(14,'l',NULL,'$2b$10$.dOKAumd7kwBCZf.yKxyVuZMNm2JjKb1iGn0ADn70ZdXC8LSdXi3O','71999999999','TATAT','88888888888'),(15,'s',NULL,'$2b$10$6.CsHjyC1IHJixFYbDnLp.mHmyUxhERyjI2boEf/DmXVGelCmh.qC','81999999999','r','11111111111'),(18,'lua',NULL,'$2b$10$ubaKxCO9Dw9ZuVuEwPPxduE8.zrvw9qzjWk.GmfHUqnUsZ25lBL4K','553199120697','q','51994179830'),(19,'joao henrique',NULL,'$2b$10$dH1CbNilTUySgfQxLq5Ave5hldhINw/ZMC.xjKEg5hVOTI/7FMFiq','5511945043522','sdsd','44444444444'),(20,'savio angelo',NULL,'$2b$10$.iL1xzmF27OwZ62Dm7nw1utte5EeTTjdfEahjdtouRr8C5hJQRkIi','553196419022','Rua ipanema 147','16458285644');
+INSERT INTO `usuario` VALUES (11,'julia freitas',NULL,'$2b$10$7AG63EzNWB3sMK.kmrwzVOXJrXy.W4qQDtdOeT7je1EbiSMtRcrve','31988005544','rua nao sei das quantas','78978978978'),(12,'leandra arruda',NULL,'$2b$10$uBQwEgTYQwB7WAZxw3WQ5O/1o5JEU0pdnSyl8h2xRJFf9K7EakFSG','41999999999','tqatatata','12312312312'),(13,'juliana dark',NULL,'$2b$10$iRycEJed8AmVYC59pLV2A.XDeEDVuS7.zB/Bxqtkw39cTfeIuO6Vq','51999999999','tatatat','67867867867'),(14,'l',NULL,'$2b$10$.dOKAumd7kwBCZf.yKxyVuZMNm2JjKb1iGn0ADn70ZdXC8LSdXi3O','71999999999','TATAT','88888888888'),(15,'s',NULL,'$2b$10$6.CsHjyC1IHJixFYbDnLp.mHmyUxhERyjI2boEf/DmXVGelCmh.qC','81999999999','r','11111111111'),(19,'joao henrique',NULL,'$2b$10$dH1CbNilTUySgfQxLq5Ave5hldhINw/ZMC.xjKEg5hVOTI/7FMFiq','5511945043522','sdsd','44444444444'),(20,'savio angelo',NULL,'$2b$10$.iL1xzmF27OwZ62Dm7nw1utte5EeTTjdfEahjdtouRr8C5hJQRkIi','553196419022','Rua ipanema 147','16458285644'),(21,'Luana Fialho da Silva','luana3@gmail.com','$2b$10$w/ehK5cuiH52qQHdAseZguyTh.Qjk5NQarXux5ALvt20ZoOX8cjX6','553199120697','benjamin neves 69 centro','51994179830');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -195,4 +178,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-20 21:46:41
+-- Dump completed on 2025-06-23  2:23:43
